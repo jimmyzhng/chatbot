e7ce67d3e30c334257c35e7e5856dfc846706fc7
Jimmy Zhang <jimmyzhang1@hotmail.com>
2024-11-07 18:28:27 -0800

    Update message post route to include gpt query

--- routes/messageRoutes.ts
+++ routes/messageRoutes.ts
@@ -1,7 +1,25 @@
 import express, { Router } from "express";
+import { Document, VectorStoreIndex, SimpleDirectoryReader, Settings, OpenAI } from "llamaindex";
+import { config } from 'dotenv';
 import pool from "../db";
 
 const router: Router = express.Router();
+
+Settings.llm = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    model: "gpt-4o-mini",
+  });
+
+  // Load and index documents
+async function initializeIndex() {
+
+    const documents = await new SimpleDirectoryReader().loadData('../data')
+    
+    // splits text, creates embedding, then stores them in VectorStoreIndex
+    const index = await VectorStoreIndex.fromDocuments(documents)
+    return index.asQueryEngine()
+}
+
 
 router.get("/", async (req, res) => {
   const { id } = req.body;
@@ -21,24 +39,36 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-  const { message, sender, user_id } = req.body;
+// Update DB + Query GPT
+router.post("/", async (req, res) => {  
+    try {
+       const { message, sender, user_id } = req.body;
+     
+       if (!message || typeof message !== 'string') {
+         res.status(400).json({ error: 'Message is required.' });
+         return;
+        }
+       const dbResult = await pool.query(
+           `
+           INSERT INTO messages
+           (message, sender, user_id)
+           VALUES ($1 $2 $3)
+           
+           `,
+           [message, sender, user_id]
+        );
+        
+    const queryEngine = await initializeIndex();
+    const chatResponse = await queryEngine.query({ query: `
+        Answer as a personal assistant who knows all about Jimmy Zhang. Be polite, personable, 
+        and answer as if you're helping someone learn about Jimmy. Answer in a conversation style, 
+        and only answer questions that are asked. Query: "${message}` });
+    res.send({ chatResponse: chatResponse.toString() });
 
-  try {
-    const result = await pool.query(
-      `
-            INSERT INTO messages
-            (message, sender, user_id)
-            VALUES ($1 $2 $3)
-            
-            `,
-      [message, sender, user_id]
-    );
-    
-    res.status(201).json(result.rows[0]);
   } catch (err) {
-    console.error("Database query error:", err);
-    res.status(500).send("Database Error");
+    console.error("Server error:", err);
+    res.status(500).send("Server Error");
   }
 });
+
 export default router;
--- server.ts
+++ server.ts
@@ -1,16 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
-import { Document, VectorStoreIndex, SimpleDirectoryReader, Settings, OpenAI } from "llamaindex";
 import { config } from 'dotenv';
 
 import userRoutes from './routes/userRoutes' 
+import messageRoutes from './routes/messageRoutes'
 
 config();
-
-Settings.llm = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-    model: "gpt-4o-mini",
-  });
 
 const app: Express = express();
 const port = process.env.PORT || 3005;
@@ -20,38 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/user', userRoutes)
-
-// Load and index documents
-async function initializeIndex() {
-
-    const documents = await new SimpleDirectoryReader().loadData('./data')
-    
-    // splits text, creates embedding, then stores them in VectorStoreIndex
-    const index = await VectorStoreIndex.fromDocuments(documents)
-    return index.asQueryEngine()
-}
-
-
-app.post('/query', async (req: Request, res: Response): Promise<void> => {
-
-    try {
-        const { query } = req.body;
-
-        if (!query || typeof query !== 'string') {
-         res.status(400).json({ error: 'Query is required.' });
-         return;
-        }
-
-        const queryEngine = await initializeIndex();
-    
-        const response = await queryEngine.query({ query: `Answer as a personal assistant who knows all about Jimmy Zhang. Be polite, personable, and answer as if you're helping someone learn about Jimmy. Query: "${query}` });
-         res.send({ response: response.toString() });
-        
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal server error. Please try again.' });
-    }
-})
+app.use('/message', messageRoutes)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
